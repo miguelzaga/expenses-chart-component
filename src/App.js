9fc9ca644eb0d949a04e1c7ad4de97f32f019d9b
Miguel <miguezaga2@hotmail.com>
2022-11-12 21:46:53 -0500

    add html tags

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <h1>New App</h1>
+      <header>
+        <h2>My balance</h2>
+        <b>$921.48</b>
+        <img src="" alt="logo" />
+      </header>
+      <main>
+        <h1>Spending - Last 7 days</h1>
+        <div>mon tue wed thu fri sat sun</div>
+        <footer>
+          <div>
+            <h3>Total this month</h3>
+            <b>$478.33</b>
+          </div>
+
+          <div>
+            <p>+2.4%</p>
+            <h3>from last month</h3>
+          </div>
+        </footer>
+      </main>
     </div>
   );
 }
